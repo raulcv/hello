8ee209386ead63c9035e7516007548150ef7a39d
Raul <iraulcv@gmail.com>
2022-01-06 23:01:58 -0500

    Adding get one user by id

--- src/routes/user.js
+++ src/routes/user.js
@@ -51,4 +51,44 @@ router.get("/", async (req, res) => {
       console.log(err);
     }
   });
+router.get("/One/:id", async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      console.log(user);
+      res.status(201).json(user);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+router.put("/Update/:id", async (req, res) => {
+    try {
+      const { first_name, last_name, email, password } = req.body;
+      if (!(email && password && first_name && last_name)) {
+        res.status(400).send("All input is required");
+      }
+      // check if user already exist
+      // Validate if user exist in our database
+      const oldUser = await User.findOne({ email });
+      if (oldUser) {
+        return res.status(409).send("User Already Exist. Please Login");
+      }
+  
+      //Encrypt user password with dcrypt package
+      encryptedPassword = await bcrypt.hash(password, 10);
+  
+      //find and update
+      const user = await User.findByIdAndUpdate(req.params.id, {firstname: first_name, lastname: last_name });
+  
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        { expiresIn: "2h" }
+      );
+      user.token = token;
+      res.status(201).json(user);
+    } catch (err) {
+      console.log(err);
+    }
+  });
 module.exports = router;
