6bf601d44bdcbf516ca5eeab21e63c75ac32aea3
Raul <iraul@gmail.com>
2022-01-08 03:47:38 -0500

    upgrade the DB conection on my mac

--- src/config/database.js
+++ src/config/database.js
@@ -2,13 +2,18 @@ const mongoose = require("mongoose");
 // const { database } = require("./key");
 // require('dotenv').config();
 // console.log(process.env.DBCON)
-let databaseurl = ""
+const username = process.env.USER_NAME;
+const password = process.env.PASSWORD_DB;
+const cluster = process.env.CLUSTER;
+const dbname = process.env.DB_NAME;
+
+const conectionString = MONGO_URI=`mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`
 const conexion = async () => {
     // console.log("database: " + database.DBCON);
     console.log("*******************************************************************");
-    console.log(process.env.MONGO_URI);
+    console.log(process.env.PASSWORD_DB);
     console.log("*******************************************************************");
-  await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+  await mongoose.connect(conectionString, { useNewUrlParser: true });
 };
 conexion();
 
