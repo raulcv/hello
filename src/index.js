70d769cb3bbc24f1cfb3c8ad23f36676a221048e
Raul CV <51675717+raulcv@users.noreply.github.com>
2022-03-10 23:35:12 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-require('./config/database.js');
 const app = require('./app')
-let hostname = "localhost";
+
 app.use(require("./routes/index"));
-app.use('/api/bears', require('./routes/bear')); 
-app.use('/api/users', require('./routes/user'));
 
 //starting server
 app.listen(app.get("port"), () => {
-  console.log(`server listeting on port http://${hostname}:${app.get("port")}/`);
+  console.log(`server listeting on port ${app.get("port")}`);
 });
+
