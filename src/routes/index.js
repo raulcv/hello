f7f15a1dcc3d178ee187a4f4f6fb01e7504e1bf6
Raul CV <51675717+raulcv@users.noreply.github.com>
2022-03-10 23:33:46 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,8 @@ const { Router } = require('express');
 const router = Router();
 
 //Raiz
-router.get('/', (req, res) => {    
-    res.json(
-        {
-            "Title": "Hola mundo con express en AWS EC2!"
-        }
-    );
-})
+
+//Raiz
+router.get('/', (req, res) => {res.json("Holiii")})
 
 module.exports = router;
