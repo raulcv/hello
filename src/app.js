549ad889f013fce4a5b99f37ce823771893d052f
Raul CV <51675717+raulcv@users.noreply.github.com>
2022-03-10 23:34:38 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,11 @@
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
 const path = require('path')
-// require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-// require('dotenv').config();
-const serverConfig = require('../src/config/serverConfig.js')
-const APP_PORT = serverConfig.app.port;
-//settings
-app.set("port", APP_PORT);
+
+app.set("port", API_PORT || 8080);
 app.set("json spaces", 2);
 
 //Middleware
-app.use(morgan());
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 module.exports = app;
